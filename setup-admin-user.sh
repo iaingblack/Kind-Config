89ed40f06383a92d6bbf4b33b5a1b4b2361e3d5b
Iain Black <iain@rootisgod.com>
2021-11-09 15:25:02 +0000

    Update setup-admin-user.sh

--- setup-admin-user.sh
+++ setup-admin-user.sh
@@ -4,3 +4,5 @@ echo '- - - - - - - - - - - - - - - - - - - - - - - - - -'
 kubectl get -n kube-system secret $(kubectl get -n kube-system sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}" | tee token.txt
 echo 
 echo '- - - - - - - - - - - - - - - - - - - - - - - - - -'
+# Rancher User
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user admin-user
